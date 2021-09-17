a9415728570c8e2fba331a339bf8d163341154b7
giapnb2 <nguyenbagiap_t57@hus.edu.vn>
2021-09-17 16:39:42 +0700

    -- ins 47

--- src/uies/templates/_payment/PaymentSuccess.jsx
+++ src/uies/templates/_payment/PaymentSuccess.jsx
@@ -10,14 +10,18 @@ class PaymentSuccess extends React.Component {
           <div className="img-fluid d-flex justify-content-center pt-5">
             <img src={paymentSuccess} alt="img-success" />
           </div>
-          <div className="note text-center pt-5 d-flex flex-column pb-5">
-            <h3 className="font-weight-bold">
-              Quý khách đã thanh toán thành công.
+          <div className="note-success text-center pt-5 d-flex flex-column pb-5">
+            <h3 className="font-weight-bold mb-4">
+              Chúc mừng Quý khách đã thanh toán thành công!
             </h3>
             <h4>
-              Giấy chứng nhận và thông báo bảo hiểm sẽ được gửi tới email của
-              Quý khách.
+              Giấy chứng nhận bảo hiểm sẽ được gửi tới email của Quý khách.
             </h4>
+            <h4>
+              Trong trường hợp không nhận được email thông báo trong vòng 24
+              giờ, Quý khách vui lòng liên hệ hotline 1900 54 54 15.
+            </h4>
+            <h4>Cám ơn Quý khách!</h4>
           </div>
         </PaymentTemplate>
       </React.Fragment>
